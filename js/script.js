b12e7bdf58e63663de0232e40cd99384c31532c7
Rushil Chawla <rushilchawla12@gmail.com>
2021-09-09 23:47:52 +0800

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 20)
-        $(".navbar").addClass("sticky");
-        else 
-        $(".navbar").removeClass("sticky");
+        if(this.scrollY > 20){
+            $(".navbar").addClass("sticky");
+            $(".goTop").fadeIn(); 
+        }
+    
+        else {
+           $(".navbar").removeClass("sticky");
+           $(".goTop").fadeOut(); 
+        }
     });
+
+    $(".goTop").click(function(){scroll(0,0)})
 
     $('.menu-toggler').click(function(){
         $(this).toggleClass("active");
